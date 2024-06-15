49f32717af9304d782bff36ea82f52cd253da35b
Ya'Sin Figuelia <yasin.figuelia@spoonconsulting.com>
2024-06-15 21:52:16 +0300

    Finish Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   string game="yes";
+       while(game=="yes"){
+
+
+
+
+      
+        //Generate the random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        //Starting the game
+        Console.WriteLine("Get ready to guess!");
+        string guessFromUser;
+        int guess;
+        int counter = 0;
+        //Game loop
+        do{
+
+          Console.Write("What is your guess? ");
+          guessFromUser=Console.ReadLine();
+          guess = int.Parse(guessFromUser);
+          counter=counter+1;
+
+          if(guess > magicNumber){
+            Console.WriteLine("Lower");
+          }
+          else if(guess < magicNumber){
+            Console.WriteLine("Higher");
+          }
+        }
+        while(guess != magicNumber);
+        
+        //Print the success game with the counter 
+        Console.WriteLine($"Congratulation you guessed at the {counter} times");
+
+        Console.WriteLine("Do you want to play again? (yes/no) ");
+        game=Console.ReadLine();
+
+         }
     }
 }
