c5e31c4b2214d33eb88f4125da627b2df54e3898
Ya'Sin Figuelia <yasin.figuelia@spoonconsulting.com>
2024-06-15 20:10:16 +0300

    Finish Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   // Print the asking first name question and get the answer 
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        // Print the asking last name question and get the answer 
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
