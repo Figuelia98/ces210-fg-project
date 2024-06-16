429966d5d2aeb424b55b4a9fb8f03140932339a4
Ya'Sin Figuelia <yasin.figuelia@spoonconsulting.com>
2024-06-16 06:52:30 +0300

    Finish Perp 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+      DisplayWelcome();
+     string userName = PromptUserName();
+     int userNumber= PromtUserNumber();
+
+     DisplayResult(userName,userNumber);
     }
+    static void DisplayWelcome(){
+
+        Console.WriteLine("Welcome to the program");
+    }
+    static string PromptUserName(){
+        
+        Console.Write("Please enter your name:  ");
+        string userName=Console.ReadLine();
+
+        return userName;
+
+    }
+
+    static int PromtUserNumber(){
+
+         
+        Console.Write("Please enter your favorite number:  ");
+        string userNumber=Console.ReadLine();
+        int number = int.Parse(userNumber);
+       
+        return number;
+    }
+    static void DisplayResult(string name,int number){
+
+        Console.WriteLine($"{name}, the square of your number is {number*number} ");
+    }
+
 }
